353e59cdc941e1e0d833a4d58b41488793940964
dagi12 <dagi12@o2.pl>
2016-08-07 00:59:21 +0200

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function (grunt) {
+
+    var script = grunt.file.read("dist/script.js");
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            dist: {
+                files: {
+                    'dist/style.min.css': ['styles/*.css']
+                }
+            }
+        },
+        replace: {
+            dist: {
+                options: {
+                    patterns: [{
+                        match: 'myscript',
+                        replacement: script
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['index.html'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+        processhtml: {
+            dist: {
+                options: {
+                    process: true,
+                    data: {
+                        title: 'My app',
+                        message: 'This is production distribution'
+                    }
+                },
+                files: {
+                    'dist/index.html': ['dist/index.html']
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.registerTask('default', ['cssmin', 'replace', 'processhtml', 'htmlmin']);
+
+};
